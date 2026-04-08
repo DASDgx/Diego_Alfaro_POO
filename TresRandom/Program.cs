126c824c1a41da04f0716bde24be3ecfaf109321
DASDgx <diegomine04011@gmail.com>
2026-04-07 20:52:53 -0600

    Criar TresRandom/Program.cs
    Generar 3 valores random que juntos sumen 100, imprimiendo un emoji diferente por cada valor generado

---
+++ TresRandom/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+
+namespace TresRandom;
+
+public static class Program {
+    public static void Main() {
+        Random rnd = new Random();
+        int total = 100;
+        string emooo = "🙋‍♂️";
+        int chekeo = 1;
+
+        int valorUno = rnd.Next(0, 101);
+        Console.WriteLine($"Valor generado {valorUno}");
+        total -= valorUno;
+
+        int valorDos = rnd.Next(0, total);
+        Console.WriteLine($"Valor generado {valorDos}");
+        total -= valorDos;
+
+        Console.WriteLine($"Valor generado {total}");
+        Console.WriteLine($"{valorUno} + {valorDos} + {total} = 100");
+
+        int turno = valorUno;
+        Console.WriteLine($"TURNO DE {valorUno}");
+
+        for (int recorrido = 1; chekeo < 4; recorrido++) {
+
+            if (recorrido == turno + 1 && chekeo == 1) {
+                Console.WriteLine($"TURNO DE {valorDos}");
+                turno = valorDos;
+                recorrido = 1;
+                emooo = "🤫";
+                chekeo++;
+            }
+            if (recorrido == turno + 1 && chekeo == 2) {
+                Console.WriteLine($"TURNO DE {total}");
+                turno = total;
+                recorrido = 1;
+                emooo = "😡";
+                chekeo++;
+            }
+            if (recorrido == turno + 1 && chekeo == 3) break;
+
+            Console.WriteLine($"{recorrido} {emooo}");
+        }
+    }
+}
