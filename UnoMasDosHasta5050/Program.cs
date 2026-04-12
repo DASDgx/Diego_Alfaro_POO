16ee5efac76b769bafcb282042bf222b53bee90c
DASDgx <diegomine04011@gmail.com>
2026-04-11 18:32:28 -0600

    Criar UnoMasDosHasta5050/Program.cs
    El programa debe realizar 100 sumas hasta llegar a 5050
    
    Ejemplo
    1 + 2 = 3
    3 + 3 = 6
    6 + 4 = 10
    ...
    ..+ 100 = 5050

---
+++ UnoMasDosHasta5050/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+
+namespace UnoMasDosHasta100;
+
+public static class Program {
+    public static void Main() {
+
+        int cumulo = 1;
+        for (int cont = 2; cont < 101; cont++) {
+            Console.WriteLine($"{cumulo}+{cont} = " + (cumulo + cont));
+            cumulo += cont;
+
+        }
+    }
+}
