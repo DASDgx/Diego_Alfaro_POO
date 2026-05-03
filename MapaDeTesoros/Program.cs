9f3ae4777982d240acadea57b045025653d215ab
rzk <diegomine04011@gmail.com>
2026-05-02 21:48:32 -0600

    Se elimino un ciclo For, realizando todo en uno solo, se cambio el formato de coordenadas (X:,Y: ->  [X,Y])

--- MapaDeTesoros/Program.cs
+++ MapaDeTesoros/Program.cs
@@ -15,18 +15,13 @@ public class Program{
             for (int filas=0; filas<mapa.GetLength(0); filas++){ //Recorrido X
                 for(int columnas=0; columnas<mapa.GetLength(1); columnas++){ //Recorrido Y
                     mapa[filas,columnas] = generador.Next(0,2); //Generador de 0 y 1
-                    if(mapa[filas,columnas] == 1) contadorTesoros++; //Si genero un tesoro/1, lo cuenta
+                    if(mapa[filas,columnas] == 1){
+                        contadorTesoros++; //Si genero un tesoro/1, lo cuenta
+                        ubicaciones += $"{contadorTesoros}-[{filas},{columnas}]\n"; //Acumula la coordenada en String dentro de la variable
+                        mapaResultado += "| x "; //Acumula la casilla con el Tesoro representado en X
+                    }else mapaResultado += "|   "; //Acumula la casilla vacia / sin tesoro
                 }
-            }
-            //Lectura de Datos
-            for(int filasRead=0; filasRead<mapa.GetLength(0); filasRead++){ //Recorrido X
-                for(int columnasRead=0; columnasRead<mapa.GetLength(1); columnasRead++){ //Recorrido Y
-                    if(mapa[filasRead,columnasRead] == 1) { //Si en el arreglo mapa[X, Y] hay un 1
-                        ubicaciones += $"Y:{filasRead}, X:{columnasRead}\n"; //Acumula la coordenada en String dentro de la variable
-                        mapaResultado += "| x "; //Acumula en la variable mapa la senalizacion con X de que es un tesoro
-                    }else mapaResultado += "|   "; //Sino, Acumula en la variable mapa un espacio vacio, de que no hay tesoro
-                }
-                mapaResultado+="|\n"; //Cierra la ultima casilla del mapa
+                mapaResultado+="|\n"; //Cierra la ultima casilla de la fila
             }
             Console.WriteLine(mapaResultado); //Imprime el mapa (|  | x |  |...)
             Console.WriteLine("Numero de Tesoros: "+contadorTesoros); //Imprime conteo de Tesoros
